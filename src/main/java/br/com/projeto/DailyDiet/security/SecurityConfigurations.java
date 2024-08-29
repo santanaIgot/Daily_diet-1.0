9141afce618b373b6777da072a88ca7ec184bfa0
santanaIgot <santanaigoor01@gmail.com>
2024-08-28 23:42:18 -0300

    [ADD] - Metodo Authenticator manager

--- src/main/java/br/com/projeto/DailyDiet/security/SecurityConfigurations.java
+++ src/main/java/br/com/projeto/DailyDiet/security/SecurityConfigurations.java
@@ -2,6 +2,8 @@ package br.com.projeto.DailyDiet.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -19,8 +21,15 @@ public class SecurityConfigurations {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
+
         return new BCryptPasswordEncoder();
     }
 
